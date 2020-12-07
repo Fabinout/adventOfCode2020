b8908ea5ee0dbb90e3fb9d0661366e4d85d05a04
Fabien Lamarque <fabien.lamarque@gmail.com>
2020-12-07 22:47:23 +0100

    part2 + part1

--- day7_python/day7.py
+++ day7_python/day7.py
@@ -1,27 +1,69 @@
-from day7_python.input_test import test_input
+import string
+import re
 
 
-def extract_name(input: str) -> str:
-    return ' '.join(input.split(" bag")[0].split()[1:])
+class bag:
+    qty = []
+    child = []
+    containGold = False;
+
+    def __init__(self, str):
+        self.qty = [];
+        self.child = [];
+        self.color = str[0];
+        for b in str[1:len(str)]:
+            if (re.search("no other", b) != None):
+                break;
+            self.qty.append(int(b[0]))
+            self.child.append(b[2:len(b)])
+
+    def contains(self, collection, toplevel):
+
+        out = 1;
+        if (toplevel):
+            out = 0;
+        if (len(self.qty) == 0):
+            return 1
+        for i in range(len(self.qty)):
+            for b in collection:
+                if (b.color == self.child[i]):
+                    out += self.qty[i] * b.contains(collection, False)
+                    break
+        return out
+
+    def define(self):
+        print(self.color);
+        print("->")
+        print(len(b.child))
+        for i in range(len(self.qty)):
+            print(self.child[i])
 
 
-colors = []
-available_colors = set({})
-changed = True
-while changed:
-    print(f'{available_colors}')
-    changed = False
-    for str in test_input:
-        contenant = str.split(';')[0].split(" bags")[0]
-        colors.append(contenant)
-        contenus = str.split(';')[1].split(',')
-        for contenu in contenus:
-            if contenant not in available_colors and "shiny gold" in contenu:
-                available_colors.add(contenant)
-                changed = True
-            if extract_name(contenu) in available_colors and contenant not in available_colors:
-                print(f'{contenant=}')
-                available_colors.add(contenant)
-                changed = True
+def findContainer(collection, color):
+    for b in collection:
+        for bc in b.child:
+            if (bc == color and not b.containGold):
+                b.containGold = True;
+                findContainer(collection, b.color)
 
-                print(f'{available_colors}')
+
+file = open("original_input.txt")
+str = file.readline()
+bags = []
+
+while (str):
+    str = re.split(" bags contain | bag, | bags, ", str)
+    str[-1] = re.sub(" bags.\n| bag.\n", '', str[-1])
+    bags.append(bag(str))
+    str = file.readline()
+findContainer(bags, "shiny gold")
+out = 0
+out2 = 0;
+for b in bags:
+    if (b.color == "shiny gold"):
+        out2 = b.contains(bags, True)
+    if (b.containGold):
+        out += 1
+
+print("part 1: ", out)
+print("part 2: ", out2)
