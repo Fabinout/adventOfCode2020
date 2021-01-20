b9bbf0396caa926e16e4b3f3b3f72d08ab167cfb
Fabien Lamarque <fabien.lamarque@gmail.com>
2021-01-20 23:23:45 +0100

    presque fini

--- day14/Day14.py
+++ day14/Day14.py
@@ -1,7 +1,7 @@
 import unittest
 from typing import List
 
-from day14.input_test import test_input, test_input2, test_prod
+from day14.input_test import test_input, test_input2, test_prod, test_input3
 
 
 def to_int(n: List[int]) -> int:
@@ -29,6 +29,20 @@ class Mask(object):
         out = to_int(n)
         return out
 
+    def apply2(self, address: int) -> str:
+        l = bitfield(address)
+        n: List[str] = []
+        for index, i in enumerate(list(self.arrays)):
+            if i == "0":
+                n.append(l[index])
+            else:
+                if i == "1":
+                    n.append("1")
+                else:
+                    if i == "X":
+                        n.append("X")
+        return "".join(n)
+
 
 def bitfield(n) -> List:
     return list(format(n, '036b'))
@@ -53,12 +67,59 @@ def extract_index(s: str) -> int:
     return int(s.split("[")[1].split(']')[0])
 
 
+def compute_floatings(m: Mask, address: int) -> List[int]:
+    floating_string = m.apply2(address)
+    possible_values: List[int] = []
+    n = 0
+    floating_bits = []
+    # print(f'{floating_string}')
+    for index, i in enumerate(list(floating_string[::-1])):
+        # print(f'{index=};{i=}')
+        if "X" == i:
+            floating_bits.append(pow(2, index))
+        else:
+            if "1" == i:
+                n += pow(2, index)
+    print(f'{n=};{floating_bits=}')
+    possible_values.append(n)
+    for bit in floating_bits:
+        new_possible_values = []
+        for numbers in possible_values:
+            new_possible_values.append(numbers + bit)
+        possible_values = possible_values + new_possible_values
+
+    return possible_values
+
+
+def seaport_computation_system2(input: str):
+    mask = Mask('')
+    s: str
+    mem = {}
+    for s in test_input:
+        if s.startswith('mask'):
+            mask = Mask(s.split()[2])
+        else:
+            index = extract_index(s)
+            value = int(s.split()[2])
+            floatings_values = compute_floatings(mask, value)
+            for memory_adress in floatings_values:
+                mem[memory_adress] = value
+
+    return sum(mem.values())
+
+
 class MyTestCase(unittest.TestCase):
 
     def test_extract_index(self):
         self.assertEqual(80, extract_index("mem[80] = 11"))
         self.assertEqual(85, extract_index("mem[85] = 11"))
         self.assertEqual(8980, extract_index("mem[8980] = 11"))
+
+    def test_compute_floatings(self):
+        m = Mask("000000000000000000000000000000X1001X")
+        self.assertEqual("000000000000000000000000000000X1101X", m.apply2(42))
+
+        self.assertEqual([26, 27, 58, 59], compute_floatings(m, 42))
 
     def test_bitfield(self):
         self.assertEqual(bitfield(11), list('000000000000000000000000000000001011'))
@@ -78,6 +139,9 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(seaport_computation_system(test_input), 165)
         self.assertEqual(seaport_computation_system(test_input2), 238)
         self.assertEqual(165, seaport_computation_system(test_prod))
+
+    def test_seaport_computation_system2(self):
+        self.assertEqual(seaport_computation_system2(test_input3), 208)
 
 
 if __name__ == '__main__':
